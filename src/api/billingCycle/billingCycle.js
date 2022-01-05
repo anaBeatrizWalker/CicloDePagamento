7b9b3e2a598be41c168458c498fd5ba4c03c1237
Ana Beatriz <anabiawalker@gmail.com>
2022-01-05 15:12:28 -0300

    Adicionando mapeamento para o banco de dados

---
+++ src/api/billingCycle/billingCycle.js
@@ -0,0 +1,24 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+//Esquema relacionado ao crédito (salário, pagamentos recebidos)
+const creditSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true }
+})
+//Esquema relacionado ao débito (pagamentos a fazer)
+const debtSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true },
+    status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
+})
+//Esquema do próprio ciclo de pagamento
+const billingCycleSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1970, max: 2100, required: false },
+    credits: [creditSchema],
+    debts: [debtSchema]
+})
+
+module.exports = restful.model('BillingCycle', billingCycleSchema)
